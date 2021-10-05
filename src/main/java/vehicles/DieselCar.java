c238263e80d2d98aeadd97ca5d648f83a49739b1
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-05 13:50:52 +0100

    made diesel car, has gearbox

--- src/main/java/vehicles/DieselCar.java
+++ src/main/java/vehicles/DieselCar.java
@@ -1,4 +1,16 @@
 package vehicles;
 
-public class DieselCar {
+import parts.Chassis;
+import parts.Gearbox;
+import parts.Tyres;
+import parts.Engine;
+
+public class DieselCar extends Vehicle{
+
+    public DieselCar(Tyres tyres, Gearbox gearbox, Chassis chassis, String colour, int price) {
+        super(tyres, gearbox, chassis, colour, price);
+        this.engine = Engine.DIESEL;
+    }
+
+
 }
--- src/test/java/vehiclesTest/DieselCarTest.java
+++ src/test/java/vehiclesTest/DieselCarTest.java
@@ -1,4 +1,37 @@
 package vehiclesTest;
 
+import org.junit.Before;
+import org.junit.Test;
+import parts.Chassis;
+import parts.Gearbox;
+import parts.Tyres;
+import vehicles.DieselCar;
+import vehicles.ElectricCar;
+
+import static org.junit.Assert.assertEquals;
+
 public class DieselCarTest {
+
+    Chassis chassis;
+    DieselCar dieselCar;
+    Tyres tyres;
+    Gearbox gearbox;
+    String colour;
+    int price;
+
+    @Before
+    public void before() {
+        tyres = Tyres.ALLWEATHER;
+        gearbox = Gearbox.AUTOMATIC;
+        chassis = Chassis.SUV;
+        colour = "Red";
+        price = 20000;
+
+        dieselCar = new DieselCar(tyres, gearbox, chassis, colour, price);
+    }
+
+    @Test
+    public void dieselCarHasGearbox() {
+        assertEquals(Gearbox.AUTOMATIC, dieselCar.getGearbox());
+    }
 }
