82136b7fa48381335bcd55c8fe0768b5100c3caa
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-05 13:59:10 +0100

    made hyrid car, has chassis

--- src/main/java/vehicles/HybridCar.java
+++ src/main/java/vehicles/HybridCar.java
@@ -1,4 +1,14 @@
 package vehicles;
 
-public class HybridCar {
+import parts.Chassis;
+import parts.Engine;
+import parts.Gearbox;
+import parts.Tyres;
+
+public class HybridCar extends Vehicle{
+
+    public HybridCar(Tyres tyres, Gearbox gearbox, Chassis chassis, String colour, int price) {
+        super(tyres, gearbox, chassis, colour, price);
+        this.engine = Engine.HYBRID;
+    }
 }
--- src/test/java/vehiclesTest/HybridCarTest.java
+++ src/test/java/vehiclesTest/HybridCarTest.java
@@ -1,4 +1,38 @@
 package vehiclesTest;
 
+import org.junit.Before;
+import org.junit.Test;
+import parts.Chassis;
+import parts.Engine;
+import parts.Gearbox;
+import parts.Tyres;
+import vehicles.HybridCar;
+
+
+import static org.junit.Assert.assertEquals;
+
 public class HybridCarTest {
+
+    Chassis chassis;
+    HybridCar hybridCar;
+    Tyres tyres;
+    Gearbox gearbox;
+    String colour;
+    int price;
+
+    @Before
+    public void before() {
+        tyres = Tyres.ALLWEATHER;
+        gearbox = Gearbox.AUTOMATIC;
+        chassis = Chassis.SUV;
+        colour = "Red";
+        price = 20000;
+
+        hybridCar = new HybridCar(tyres, gearbox, chassis, colour, price);
+    }
+
+    @Test
+    public void HybridCarHasChassis() {
+        assertEquals(Chassis.SUV, hybridCar.getChassis());
+    }
 }
