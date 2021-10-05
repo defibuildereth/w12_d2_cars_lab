2eadc0c53971a1066ef6767d0f4879eb57ee1780
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-05 13:55:48 +0100

    made petrol car, has engine

--- src/main/java/vehicles/PetrolCar.java
+++ src/main/java/vehicles/PetrolCar.java
@@ -1,4 +1,14 @@
 package vehicles;
 
-public class PetrolCar {
+import parts.Chassis;
+import parts.Gearbox;
+import parts.Tyres;
+import parts.Engine;
+
+public class PetrolCar extends Vehicle{
+
+    public PetrolCar(Tyres tyres, Gearbox gearbox, Chassis chassis, String colour, int price) {
+        super(tyres, gearbox, chassis, colour, price);
+        this.engine = Engine.PETROL;
+    }
 }
--- src/test/java/vehiclesTest/PetrolCarTest.java
+++ src/test/java/vehiclesTest/PetrolCarTest.java
@@ -1,4 +1,38 @@
 package vehiclesTest;
 
+import org.junit.Before;
+import org.junit.Test;
+import parts.Chassis;
+import parts.Engine;
+import parts.Gearbox;
+import parts.Tyres;
+import vehicles.PetrolCar;
+
+
+import static org.junit.Assert.assertEquals;
+
 public class PetrolCarTest {
+
+    Chassis chassis;
+    PetrolCar petrolCar;
+    Tyres tyres;
+    Gearbox gearbox;
+    String colour;
+    int price;
+
+    @Before
+    public void before() {
+        tyres = Tyres.ALLWEATHER;
+        gearbox = Gearbox.AUTOMATIC;
+        chassis = Chassis.SUV;
+        colour = "Red";
+        price = 20000;
+
+        petrolCar = new PetrolCar(tyres, gearbox, chassis, colour, price);
+    }
+
+    @Test
+    public void petrolCarHasEngine() {
+        assertEquals(Engine.PETROL, petrolCar.getEngine());
+    }
 }
